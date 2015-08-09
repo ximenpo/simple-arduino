853c1f010907fb1b75424ecba6427070619574bf
ximenpo <ximenpo@jiandan.ren>
2015-08-09 11:42:45 +0800

    add debouncing.h for debouncing pin input [like a button].

---
+++ debouncing.h
@@ -0,0 +1,38 @@
+#include  <Arduino.h>
+#include  "procedure.h"
+#include  "timestamp.h"
+
+template<typename STAMP_TYPE = unsigned long>
+class  debouncing  : public procedure_context, timestamp_timing<STAMP_TYPE> {
+  public:
+    void  init(int pin, int expect) {
+      this->pin = pin;
+      this->expect  = expect;
+
+      pinMode(this->pin, INPUT);
+    }
+
+    bool  update(STAMP_TYPE timestamp) {
+      this->current_timestamp = timestamp;
+      PROCEDURE_BEGIN_RUNLOOP(this);
+
+      //  first trigger
+      PROCEDURE_WAIT_(digitalRead(this->pin) == this->expect, false);
+
+      //  second check
+      PROCEDURE_SLEEP_(this, 50, false);
+      if (digitalRead(this->pin) != this->expect) {
+        PROCEDURE_STOP_(false);
+      }
+
+      //  waiting for end.
+      PROCEDURE_WAIT_(digitalRead(this->pin) != this->expect, false);
+
+      PROCEDURE_END_(true);
+    }
+
+  private:
+    int   pin;
+    int   expect;
+};
+
